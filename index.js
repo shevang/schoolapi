1b1fdad80122c2af7ecdedcfbcfeef6e0b3e2ee0
Shevang Sharma <sharma.shevang@gmail.com>
2024-11-30 23:34:18 +0530

    testing3

--- index.js
+++ index.js
@@ -95,6 +95,6 @@ app.get('/listSchools', (req, res) => {
     });
 });
 
-// Start the server
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
